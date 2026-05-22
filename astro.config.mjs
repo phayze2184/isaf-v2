9740bb434a15742e5370f643d9839079e1ddd657
Ramona <rmn.neagu@yahoo.ro>
2026-05-22 21:50:40 +0300

    Deploy Astro site with GitHub Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://phayze2184.github.io",
+  base: "/isaf-v2",
+});
